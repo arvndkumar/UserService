b3ee2212848fb7afb911410b1f32affa6734c046
Arvind Kumar <arvndkumar.86@gmail.com>
2026-01-22 20:57:36 -0500

    enhancements

--- src/main/java/com/ecommerce/userservice/dto/User/RegisterRequest.java
+++ src/main/java/com/ecommerce/userservice/dto/User/RegisterRequest.java
@@ -3,5 +3,9 @@ package com.ecommerce.userservice.dto.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record RegisterRequest (@NotBlank String name, @Email String email, @NotBlank String  password) {
+public record RegisterRequest (
+
+        @NotBlank String name,
+        @Email String email,
+        @NotBlank String  password) {
 }
